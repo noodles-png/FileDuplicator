fede889a253dcc92361f83d7fba5e020cc7e4f7b
PatrickCarillo <p.carillo17@googlemail.com>
2026-06-11 23:04:15 +0200

    Removed .idea folder

--- FileDuplicator/Program.cs
+++ FileDuplicator/Program.cs
@@ -3,6 +3,6 @@
 try
 {
     string directoryPath =
-        "";
+        "C:\\Users\\PatrickCarillo\\OneDrive - BBQ - Baumann Bildung und Qualifizierung GmbH\\Dokumente\\Berichtsheft_BBQ_2026-27";
     
 }
