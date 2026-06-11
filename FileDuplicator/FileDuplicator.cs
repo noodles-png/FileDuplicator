d13f4848ab8281708a203879ffd146512d19d654
PatrickCarillo <p.carillo17@googlemail.com>
2026-06-11 22:53:24 +0200

    Initial commit

---
+++ FileDuplicator/FileDuplicator.cs
@@ -0,0 +1,18 @@
+﻿namespace FileDuplicator;
+using System.IO;
+
+
+public class FileNamer
+{
+    string _path = Directory.GetCurrentDirectory();
+    string _fileName = ;
+    private string _file = "";
+
+    public FileNamer(string path) // ToDo: Choose a better name
+    {
+        string srcFile =  Path.GetFileNameWithoutExtension(path);
+        
+        int extIndex = srcFile.LastIndexOf("kw", StringComparison.OrdinalIgnoreCase);
+        
+    }
+}
